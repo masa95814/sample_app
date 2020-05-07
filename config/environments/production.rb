0586a23a639cb9f7d80e73af9ab7c7535a035cce
masa95814 <masa95814@gmail.com>
2020-05-07 16:15:53 +0900

    fix mail address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   host = 'masa-sample-app.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => "smtp.sendgrid.net",
+    :address        => "smtp.gmail.com",
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
