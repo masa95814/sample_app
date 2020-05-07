0b0c4add087788cea440bd04a26c09c41d5c314b
masa95814 <masa95814@gmail.com>
2020-05-07 16:26:18 +0900

    add heroku pass

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address        => "smtp.gmail.com",
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'app170426650@heroku.com',
+    :password       => 'o0i5bxru3175',
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
